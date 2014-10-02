06ff71aae164675e52ca9e4f8927be4d78e2afcf
GraspinglySilver <jack@jackmagiera.com>
2014-10-02 12:08:26 -0400

    began instantiating product group

--- ProductGroup.py
+++ ProductGroup.py
@@ -1,1 +1,14 @@
 # ProductGroup.py
+class ProductGroup:
+	def __init__(self, num_items, max_spent):
+		self.itemCount = num_items
+		self.maxPrice = max_spent
+		self.totalSpent = 0
+		self.items = []
+
+	def addOne(item):
+		items.append(item)
+
+	def printAllByKey(key):
+		for item in items:
+			print item[key]
